79f2de785715920a8a9c6fc5bc89790308fa726e
Crystal Prusek <crystal.prusek@gmail.com>
2020-08-06 16:15:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,10 +8,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(5)
-  while second_to_midnight > 0 
-    puts "#{seconds_to_midnight} SECOND(S)!"
+  while number > 0 
+    puts "#{number} SECOND(S)!"
     sleep(5)
-    seconds_to_midnight -= 1 
+    number -= 1 
   end
   return "HAPPY NEW YEAR!" 
 end
